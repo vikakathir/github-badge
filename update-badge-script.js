75e5ea1d5e6d2a85884a0b68fc7324d270c8b7f6
vikakathir <24149062@drngpit.ac.in>
2026-07-17 15:42:04 +0530

    Change userName variable in update-badge-script.js

--- update-badge-script.js
+++ update-badge-script.js
@@ -1,7 +1,7 @@
 import fetch from 'node-fetch';
 import fs from 'fs';
 
-let userName = "saran-k-07";
+let userName = "vikakathir";
 
 let pipelineRun = true; // Set to false for local debugging
 let templateFile = 'github-repo-count-template.svg';
